603577ec042a1bd06824be89edb472709f97f95c
Budes <mrgoat4321@gmail.com>
2024-04-19 00:19:50 +0200

    Initial commit.

---
+++ Forme/DetaljiUcenika.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Knjiznicaaaaa.Forme
+{
+    partial class DetaljiUcenika
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbOIB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbIme = new System.Windows.Forms.TextBox();
+            this.tbPrezime = new System.Windows.Forms.TextBox();
+            this.tbAdresa = new System.Windows.Forms.TextBox();
+            this.tbTelefon = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OIB";
+            // 
+            // tbOIB
+            // 
+            this.tbOIB.Location = new System.Drawing.Point(107, 55);
+            this.tbOIB.Name = "tbOIB";
+            this.tbOIB.Size = new System.Drawing.Size(158, 20);
+            this.tbOIB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Prezime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Adresa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Telefon";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Razred";
+            // 
+            // tbIme
+            // 
+            this.tbIme.Location = new System.Drawing.Point(107, 91);
+            this.tbIme.Name = "tbIme";
+            this.tbIme.Size = new System.Drawing.Size(158, 20);
+            this.tbIme.TabIndex = 7;
+            // 
+            // tbPrezime
+            // 
+            this.tbPrezime.Location = new System.Drawing.Point(107, 131);
+            this.tbPrezime.Name = "tbPrezime";
+            this.tbPrezime.Size = new System.Drawing.Size(158, 20);
+            this.tbPrezime.TabIndex = 8;
+            // 
+            // tbAdresa
+            // 
+            this.tbAdresa.Location = new System.Drawing.Point(107, 165);
+            this.tbAdresa.Name = "tbAdresa";
+            this.tbAdresa.Size = new System.Drawing.Size(158, 20);
+            this.tbAdresa.TabIndex = 9;
+            // 
+            // tbTelefon
+            // 
+            this.tbTelefon.Location = new System.Drawing.Point(107, 201);
+            this.tbTelefon.Name = "tbTelefon";
+            this.tbTelefon.Size = new System.Drawing.Size(158, 20);
+            this.tbTelefon.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(107, 237);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(45, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(107, 278);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(120, 34);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // DetaljiUcenika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 324);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbTelefon);
+            this.Controls.Add(this.tbAdresa);
+            this.Controls.Add(this.tbPrezime);
+            this.Controls.Add(this.tbIme);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbOIB);
+            this.Controls.Add(this.label1);
+            this.Name = "DetaljiUcenika";
+            this.Text = "Detalji Učenika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbOIB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbIme;
+        private System.Windows.Forms.TextBox tbPrezime;
+        private System.Windows.Forms.TextBox tbAdresa;
+        private System.Windows.Forms.TextBox tbTelefon;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
