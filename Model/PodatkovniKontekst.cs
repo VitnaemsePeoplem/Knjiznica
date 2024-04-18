56299d6b3a3dc7f65d5e03cad992fbf6e6207b47
Budes <mrgoat4321@gmail.com>
2024-04-18 23:51:22 +0200

    Initial commit.

---
+++ Model/PodatkovniKontekst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knjiznicaaaaa.Model
+{
+    public class PodatkovniKontekst
+    {
+        //kolekcije
+        public List<Ucenik> Ucenici;
+        public List<Knjiga> Knjige;
+        public List<Posudba> Posudbe;
+    }
+}
