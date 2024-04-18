7fa3831c2d67e69788ab580bda8f159f0c65dd06
Budes <mrgoat4321@gmail.com>
2024-04-18 23:32:45 +0200

    Initial commit.

---
+++ Model/Knjiga.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knjiznicaaaaa.Model
+{
+   public class Knjiga: IComparable
+    {
+        public string ISBN { get; set; }
+        public string Autor { get; set; }
+        public string Naslov { get; set; }
+        public int GodinaIzdanja { get; set; }
+        public int BrojPrimjeraka { get; set; }
+
+       
+        public override string ToString()
+        {
+            return this.Autor + ": " + this.Naslov;
+        }
+
+        //Uspoređujemo prema autoru pa prema naslovu
+        public int CompareTo(object obj)
+        {
+            int rez = this.Autor.CompareTo(((Knjiga)obj).Autor);
+
+            if (rez == 0)
+            {
+                rez = int rez = this.Naslov.CompareTo(((Knjiga)obj).Naslov);
+            }
+
+            return rez;
+        }
+
+    }
+}
