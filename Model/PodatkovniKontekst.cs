22e90f176d747953c78481f75187e7cbdc391f7f
Budes <mrgoat4321@gmail.com>
2024-04-21 02:59:07 +0200

    ucitavanje ucenika

--- Model/PodatkovniKontekst.cs
+++ Model/PodatkovniKontekst.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Knjiznicaaaaa.Model
 {
@@ -11,5 +12,44 @@ namespace Knjiznicaaaaa.Model
         public List<Ucenik> Ucenici;
         public List<Knjiga> Knjige;
         public List<Posudba> Posudbe;
+
+        private string datUcenici = "ucenici.txt";
+        public PodatkovniKontekst()
+        {
+            Ucenici = UcitajUcenike();
+        }
+
+        public List<Ucenik> UcitajUcenike()
+        {
+            List<Ucenik> rezultat = new List<Ucenik>();
+
+            if (File.Exists(datUcenici))
+            {
+                using (StreamReader sr = new StreamReader(datUcenici))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string linija = sr.ReadLine();
+                        //djelimo liniju i definiramo objekt učenik
+                        Ucenik trenutniUcenik = new Ucenik();
+                        string[] polja = linija.Split('|');
+                        trenutniUcenik.OIB = polja[0];
+                        trenutniUcenik.Ime = polja[1];
+                        trenutniUcenik.Prezime = polja[2];
+                        trenutniUcenik.Adresa = polja[3];
+                        trenutniUcenik.Telefon = polja[4];
+                        trenutniUcenik.Razred = int.Parse(polja[5]);
+
+                        //Dodavanje u listu
+                        rezultat.Add(trenutniUcenik);
+                    }
+                }
+          }
+            return rezultat;
+        }   
+        
+
     }
+
+
 }
