cee371967ae6505b65c959229e4618606b5e72b6
Budes <mrgoat4321@gmail.com>
2024-05-02 18:06:19 +0200

    Ucitavanje ucenika i posudba u detalji posudbe

--- Forme/DetaljiPosudbe.Designer.cs
+++ Forme/DetaljiPosudbe.Designer.cs
@@ -32,12 +32,13 @@ namespace Knjiznicaaaaa.Forme
             this.lbUcenici = new System.Windows.Forms.ListBox();
             this.lbKnjige = new System.Windows.Forms.ListBox();
             this.dtpDatumPosudbe = new System.Windows.Forms.DateTimePicker();
-            this.cbBrojDana = new System.Windows.Forms.ComboBox();
             this.btnOK = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.nudBrojDana = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBrojDana)).BeginInit();
             this.SuspendLayout();
             // 
             // lbUcenici
@@ -63,46 +64,6 @@ namespace Knjiznicaaaaa.Forme
             this.dtpDatumPosudbe.Size = new System.Drawing.Size(200, 20);
             this.dtpDatumPosudbe.TabIndex = 2;
             // 
-            // cbBrojDana
-            // 
-            this.cbBrojDana.FormattingEnabled = true;
-            this.cbBrojDana.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25",
-            "26",
-            "27",
-            "28",
-            "29",
-            "30",
-            "31"});
-            this.cbBrojDana.Location = new System.Drawing.Point(425, 267);
-            this.cbBrojDana.Name = "cbBrojDana";
-            this.cbBrojDana.Size = new System.Drawing.Size(49, 21);
-            this.cbBrojDana.TabIndex = 3;
-            // 
             // btnOK
             // 
             this.btnOK.Location = new System.Drawing.Point(312, 336);
@@ -111,6 +72,7 @@ namespace Knjiznicaaaaa.Forme
             this.btnOK.TabIndex = 4;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // label1
             // 
@@ -150,23 +112,31 @@ namespace Knjiznicaaaaa.Forme
             this.label4.Text = "Broj dana";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // nudBrojDana
+            // 
+            this.nudBrojDana.Location = new System.Drawing.Point(425, 268);
+            this.nudBrojDana.Name = "nudBrojDana";
+            this.nudBrojDana.Size = new System.Drawing.Size(49, 20);
+            this.nudBrojDana.TabIndex = 9;
+            // 
             // DetaljiPosudbe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nudBrojDana);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.cbBrojDana);
             this.Controls.Add(this.dtpDatumPosudbe);
             this.Controls.Add(this.lbKnjige);
             this.Controls.Add(this.lbUcenici);
             this.Name = "DetaljiPosudbe";
             this.Text = "DetaljiPosudbe";
             this.Load += new System.EventHandler(this.DetaljiPosudbe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudBrojDana)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,11 +147,11 @@ namespace Knjiznicaaaaa.Forme
         private System.Windows.Forms.ListBox lbUcenici;
         private System.Windows.Forms.ListBox lbKnjige;
         private System.Windows.Forms.DateTimePicker dtpDatumPosudbe;
-        private System.Windows.Forms.ComboBox cbBrojDana;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudBrojDana;
     }
 }
--- Forme/DetaljiPosudbe.cs
+++ Forme/DetaljiPosudbe.cs
@@ -20,6 +20,20 @@ namespace Knjiznicaaaaa.Forme
             this.kontekst = kontekst;
         }
 
+        private void DetaljiPosudbe_Load(object sender, EventArgs e)
+        {
+            OsvjeziUcenike();
+            OsvjeziKnjige();
+
+            if ( this.Posudba != null)
+            {
+                lbUcenici.SelectedItem = this.Posudba.Ucenik;
+                lbKnjige.SelectedItem = this.Posudba.Knjiga;
+                dtpDatumPosudbe.Value = this.Posudba.DatumPosudbe;
+                nudBrojDana.Value = this.Posudba.BrojDana;
+            }
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -30,9 +44,37 @@ namespace Knjiznicaaaaa.Forme
 
         }
 
-        private void DetaljiPosudbe_Load(object sender, EventArgs e)
-        {
+        
 
+        private void OsvjeziUcenike()
+        {
+            Helper.PrikaziListuUListboxu<Ucenik>(this.kontekst.Ucenici, lbUcenici);
+        }
+
+        private void OsvjeziKnjige()
+        {
+            Helper.PrikaziListuUListboxu<Knjiga>(this.kontekst.Knjige, lbKnjige);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(lbUcenici.SelectedItem == null || lbKnjige.SelectedItem == null)
+            {
+                MessageBox.Show("Molimte odaberi ucenika i knjigu");
+                this.DialogResult = DialogResult.None;
+            }
+            else
+            {
+                if ( this.Posudba == null)
+                {
+                    this.Posudba = new Posudba();
+                }
+
+                this.Posudba.Ucenik = (Ucenik)lbUcenici.SelectedItem;
+                this.Posudba.Knjiga = (Knjiga)lbKnjige.SelectedItem;
+                this.Posudba.DatumPosudbe = dtpDatumPosudbe.Value;
+                this.Posudba.BrojDana = (int)nudBrojDana.Value;
+            }
         }
     }
 }
